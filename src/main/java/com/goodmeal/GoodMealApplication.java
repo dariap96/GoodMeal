ae09d12750ba20ade883a3fcffa1ab7a2aee966e
viktor <nav2373@mail.ru>
2021-11-02 17:37:28 +0300

    simple auth added: next step – get users from db

--- src/main/java/com/goodmeal/GoodMealApplication.java
+++ src/main/java/com/goodmeal/GoodMealApplication.java
@@ -6,14 +6,34 @@ import org.springframework.context.annotation.Import;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
+@RestController
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 @Import({TestDataLoader.class})
 public class GoodMealApplication {
 
+	@GetMapping("/")
+	public String login() {
+		return "authenticated successfully";
+	}
+
+	/*
+	@GetMapping("/home")
+	public String getUsers() {
+		return "get users page";
+	}
+	 */
+
 	public static void main(String[] args) {
-//		SpringApplication.run(GoodMealApplication.class, args);
+		SpringApplication.run(GoodMealApplication.class, args);
+	}
+
+	public static void dataLoader() {
 		System.out.println(new EdamRecipeRequest(
 				"86eec527",
 				"15ab7f74aaa32f92d53df79c9ecdc948",
@@ -22,5 +42,4 @@ public class GoodMealApplication {
 				100)
 				.sendRequest().getRecipes().get(0).getIngredientLines().size());
 	}
-
 }
