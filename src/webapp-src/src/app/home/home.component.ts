5f3ea5ac2c627f85c77befb28eaf57e0ea6a7f9e
viktor <nav2373@mail.ru>
2021-11-02 13:34:50 +0300

    new frontend added (login only)

---
+++ src/webapp-src/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RestapiService } from '../restapi.service';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+
+    users:any;
+    constructor(private service:RestapiService) { }
+
+    ngOnInit() {}
+
+    getUsers(){
+        let resp=this.service.getUsers();
+        resp.subscribe(data=>this.users=data);
+    }
+}
