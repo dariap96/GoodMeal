5188de27a12728cffa441f9ad6c129c28b19d502
Mihail <molshansky2002@gmail.com>
2021-11-19 20:39:24 +0300

    Recipe model created

---
+++ src/webapp-src/src/app/model/Recipes.ts
@@ -0,0 +1,48 @@
+import {Meals} from "./Meals";
+import {Dishes} from "./Dishes";
+import {Cuisines} from "./Cuisines";
+
+export interface Recipes {
+    data :  Datum[];
+    links : DatumLinks;
+}
+
+export interface Datum {
+    id :            number;
+    type :          Type;
+    links :         DatumLinks;
+    attributes :    Attributes;
+    relationships : Relationships;
+}
+
+export interface Attributes {
+    name : string;
+    time : number;
+    image : string;
+    actionsSequence : string;
+    originalId : string;
+}
+
+export interface DatumLinks {
+    self : string;
+}
+
+export interface Relationships {
+    meals : Meals;
+    dishes : Dishes;
+    cuisines : Cuisines;
+}
+
+export enum Type {
+    Recipe = "recipe",
+}
+
+export class ConvertRecipes {
+    public static toRecipes(json : string) : Recipes {
+        return JSON.parse(json);
+    }
+
+    public static recipesToJson(value : Recipes) : string {
+        return JSON.stringify(value);
+    }
+}
