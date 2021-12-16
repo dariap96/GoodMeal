44343d2f0174f5f9d2b9a3b59269702fb9aba8ff
Mihail <molshansky2002@gmail.com>
2021-12-16 23:51:17 +0300

    Cleaning

--- backend/src/main/java/com/goodmeal/security/SecurityConfiguration.java
+++ backend/src/main/java/com/goodmeal/security/SecurityConfiguration.java
@@ -35,12 +35,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 .authorizeRequests().antMatchers("/register", "/*.js", "/*.css", "/login", "/", "/favicon.ico").permitAll()
                 .antMatchers(
-                        "/api/user/{\\d+}",
-                                    "/update-password-by-admin/{\\d+}",
-                                    "recipe_rating/remove-by-admin/{\\d+}",
+                        "/api/user/**",
+                                    "/update-password-by-admin/**",
+                                    "recipe_rating/remove-by-admin/**",
                                     "/userinfo/{\\d+}",
-                                    "/grant-admin-access/{\\d+}",
-                                    "/disable-admin-access/{\\d+}")
+                                    "/grant-admin-access/**",
+                                    "/disable-admin-access/**")
                 .hasRole("ADMIN")
                 .anyRequest().authenticated().and().logout().invalidateHttpSession(true).logoutUrl("/logout").permitAll()
                 .and().httpBasic();
